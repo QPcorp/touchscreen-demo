7e4535748e5e819215c883e53000add232a6328d
ecavazos <eric@smartass.io>
2017-05-21 18:29:54 -0700

    update

--- app/controllers/pay-controller.js
+++ app/controllers/pay-controller.js
@@ -1,10 +1,14 @@
-app.controller('payController', function($scope, $rootScope, $location){
+app.controller('payController', function($scope, $rootScope,$timeout, $location){
 	
 	$scope.paid = false;
 
 	$scope.payAction = function(){
 		$scope.paid = true;
 	};
+
+	$timeout(function(){
+		$scope.paid = true;
+	}, 4000);
 
 	$scope.goHome = function(){
 		$location.path('/');
